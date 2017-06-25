172e85b216d7cbd18aedfea7aa6c3cb07e11bfbd
rich <morenoricardo237@gmail.com>
2017-06-25 13:17:39 -0400

    Removed semicolons from src/ajax.js

--- src/ajax.js
+++ src/ajax.js
@@ -8,88 +8,88 @@
 const ajax = {
   xhr: function () {
     try {
-      return new window.XMLHttpRequest();
+      return new window.XMLHttpRequest()
     } catch (err) {
-      throw err;
+      throw err
     }
   },
 
   query: function (config) {
-    const xhr = this.xhr();
-    const { url } = config;
-    const { method } = config;
-    const { params } = config;
-    const { fragment } = config;
+    const xhr = this.xhr()
+    const { url } = config
+    const { method } = config
+    const { params } = config
+    const { fragment } = config
 
-    let query = url;
+    let query = url
 
     if (params) {
-      query += '?';
+      query += '?'
 
       for (let key in params) {
-        query += `${key}=${params[key]}&`;
+        query += `${key}=${params[key]}&`
       }
 
-      query = query.replace(/&$/, '');
+      query = query.replace(/&$/, '')
     }
 
     if (fragment) {
-      query += `#${fragment}`;
+      query += `#${fragment}`
     }
 
-    xhr.open(method, query, true);
-    xhr.send();
+    xhr.open(method, query, true)
+    xhr.send()
 
     return new Promise(function (resolve, reject) {
       xhr.onreadystatechange = function () {
         if (this.readyState === 4) {
-          resolve(this.responseText);
+          resolve(this.responseText)
         } else if (this.status === 403 || this.status === 404) {
-          reject(`Status code ${this.status}`);
+          reject(`Status code ${this.status}`)
         }
-      };
-    });
+      }
+    })
   },
 
   get: function (url, json = false) {
-    const xhr = this.xhr();
+    const xhr = this.xhr()
 
-    xhr.open('GET', url, true);
-    xhr.send();
+    xhr.open('GET', url, true)
+    xhr.send()
 
     return new Promise(function (resolve, reject) {
       xhr.onreadystatechange = function () {
         if (this.readyState === 4) {
-          let responseText = this.responseText;
-          let response = json ? JSON.parse(responseText) : responseText;
+          let responseText = this.responseText
+          let response = json ? JSON.parse(responseText) : responseText
 
-          resolve(response);
+          resolve(response)
         } else if (this.status === 403 || this.status === 404) {
-          reject(`Status code ${this.status}`);
+          reject(`Status code ${this.status}`)
         }
-      };
-    });
+      }
+    })
   },
 
   post: function (url, data, json = false) {
-    const xhr = this.xhr();
-    data = json ? JSON.stringify(data) : data;
+    const xhr = this.xhr()
+    data = json ? JSON.stringify(data) : data
 
-    xhr.open('POST', url, true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.open('POST', url, true)
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
 
-    xhr.send(data);
+    xhr.send(data)
 
     return new Promise(function (resolve, reject) {
       xhr.onreadystatechange = function () {
         if (this.readyState === 4) {
-          resolve(this.responseText);
+          resolve(this.responseText)
         } else if (this.status === 403 || this.status === 404) {
-          reject(`Status code ${this.status}`);
+          reject(`Status code ${this.status}`)
         }
-      };
-    });
+      }
+    })
   }
-};
+}
 
-module.exports = ajax;
+module.exports = ajax
